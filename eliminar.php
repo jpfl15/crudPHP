c2cfdf755a012874bccf67464dc348097140c125
jpfl15 <50215860+jpfl15@users.noreply.github.com>
2021-11-15 17:28:27 -0700

    Add files via upload

--- eliminar.php
+++ eliminar.php
@@ -1,23 +1,20 @@
 <?php
   include ("./inc/settings.php");
   validar();
-?>
+  $id = $_POST['column1'];
 
-<?php
-$id = $_POST['column1'];
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  }
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-
-if ($conn->query("DELETE FROM table1 WHERE column1 =$id;")){
-    header("location:crud.php");
-}
-else{
-    echo "Alguio salió mal <a href='crud.php'> click aqui para volver a crud</a>";
-}
+  if ($conn->query("DELETE FROM table1 WHERE column1=$id;")){
+      header("location:crud.php");
+  }
+  else{
+      echo "Alguio salió mal <a href='crud.php'> click aqui para volver a crud</a>";
+  }
 
 ?>
