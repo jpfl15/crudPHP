da02646f22b6fdd40759d78ffc2c2980c5d5e630
jpfl15 <50215860+jpfl15@users.noreply.github.com>
2021-11-15 19:12:08 -0700

    Add files via upload

--- insertar.php
+++ insertar.php
@@ -2,22 +2,24 @@
     include ("./inc/settings.php");
     validar();
 
-    $id = $_POST ['id'];
-    $nombre = $_POST ['name'];
-    $fecha = $_POST ['fecha'];
-    $numeroD = $_POST ['numeroD'];
-    $numeroF = $_POST ['numeroF'];
+    $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname, $username, $password);
 
-    $query = "INSERT INTO table1 (column1,column2,column3,column4,column5) VALUES ($id, '$nombre', '$fecha', $numeroD, $numeroF);";
+    if (isset($_POST['name']) && isset($_POST['fecha']) && isset($_POST['numeroD']) && isset($_POST['numeroF']) && isset($_POST['id'])){
+        $id = $_POST ['id'];
+        $nombre = $_POST ['name'];
+        $fecha = $_POST ['fecha'];
+        $numeroD = $_POST ['numeroD'];
+        $numeroF = $_POST ['numeroF'];
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
-    
-    if ($conn->query($query)){
+        $query = "INSERT INTO table1 (column1,column2,column3,column4,column5) VALUES (:id, :nombre, :fecha, :numeroD, :numeroF);";
+        
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":ombre", $nombre);
+        $stmt->bindParam(":fecha", $fecha);
+        $stmt->bindParam(":numeroD", $numeroD);
+        $stmt->bindParam(":numeroF", $numeroF);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
         header("location:crud.php");
     }
     else{
