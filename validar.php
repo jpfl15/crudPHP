6e44489db46c8d960eb3d7d88ee6cb36b6b98a13
jpfl15 <50215860+jpfl15@users.noreply.github.com>
2021-11-15 18:10:14 -0700

    Add files via upload

--- validar.php
+++ validar.php
@@ -1,20 +1,24 @@
 <?php
   include ("./inc/settings.php");
-    $query = "SELECT * FROM usuarios WHERE userN = '$_POST[userN]' AND pass = md5('$_POST[pws]')";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+    $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname, $username, $password);
+
+    if (isset($_POST['userN']) && isset($_POST['pws'])){
+      $usern = $_POST['userN'];
+      $passwd = $_POST['pws'];
+      $query = "SELECT * FROM usuarios WHERE userN = :usern AND pass = md5(:passwd)";
+
+      $stmt = $pdo->prepare($query);
+      $stmt->bindParam(":usern", $usern);
+      $stmt->bindParam(":passwd", $passwd);
+      $stmt->execute();
+
     }
-
-    $result = $conn->query($query);
-
-    if ($result->num_rows > 0) {
+    
+    if ($stmt->rowCount() == 1) {
       
 
-      $row = $result->fetch_assoc();
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
       session_start();
       $_SESSION['user'] = $row['empleado_nombre'];
       $_SESSION['apellido1'] = $row['empleado_apellido1'];
