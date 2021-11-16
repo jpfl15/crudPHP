eb56c520d9aa94571b8daf214bd87f56d953627a
jpfl15 <50215860+jpfl15@users.noreply.github.com>
2021-11-15 19:06:13 -0700

    Add files via upload

--- actualizar2.php
+++ actualizar2.php
@@ -1,16 +1,25 @@
 <?php 
     include ("./inc/settings.php");
     validar();
-    $query = "UPDATE table1 SET column2 = '".$_POST['name']."', column3 = '".$_POST['fecha']."', column4 = ".$_POST['numeroD'].", column5 = ".$_POST['numeroF']." WHERE column1 = ".$_POST['id'].";";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
+    $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname, $username, $password);
 
-    if ($conn->query($query)){
+    if (isset($_POST['name']) && isset($_POST['fecha']) && isset($_POST['numeroD']) && isset($_POST['numeroF']) && isset($_POST['id'])){
+        $name = $_POST['name'];
+        $fecha = $_POST['fecha'];
+        $numeroD = $_POST['numeroD'];
+        $numeroF = $_POST['numeroF'];
+        $id = $_POST['id'];
+
+        $query = "UPDATE table1 SET column2 = :name, column3 = :fecha, column4 = :numeroD, column5 = :numeroF WHERE column1 = :id;";
+        
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":fecha", $fecha);
+        $stmt->bindParam(":numeroD", $numeroD);
+        $stmt->bindParam(":numeroF", $numeroF);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
         header("location:crud.php");
     }else{
         echo "Algo salio mal <a href='https://localhost/crud/crud.php'> clic aqui para volver al crud</a>" ;
